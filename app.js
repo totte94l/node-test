540c2aa363feb87561f8a8247b5f1001e34d219c
Christoffer Cajander <christoffer.bjorn.cajander@gmail.com>
2020-11-25 19:55:27 +0100

    Added Cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome to my app!')
