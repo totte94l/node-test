08ef9fa08dcef7185b0d2599987ec2a6defe6a4c
Christoffer Cajander <christoffer.bjorn.cajander@gmail.com>
2020-11-25 20:43:52 +0100

    lol..

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const express = require('express')
-const cors = require('cors')
-
-const PORT = process.env.PORT || 5000
-const app = express()
-
+const express = require('express');
+const app = express();
+const PORT = 5000;
 
 app.use(express.static('public'));
 
@@ -11,6 +8,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, function () {
-    console.log(`Express server listening on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
